6676a59742caa3f40c8b411c9278124ac9c1847e
Chris Tolnai <christolnai@gmail.com>
2021-09-29 15:07:35 -0700

    added dashboard file for comments

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,8 @@
 const router = require('express').Router();
-const model1Routes = require('./model1-routes');
-const model2Routes = require('./model2-routes');
 const userRoutes = require('./userRoutes')
 
 
-router.use('/model1', model1Routes);
-router.use('/model2', model2Routes);
+
 router.use('/users', userRoutes);
 
 
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -18,4 +18,23 @@ router.get('/login', (req, res) => {
   }
 });
 
+router.get('/dashboard', (req, res) => {
+  try {
+    res.render('dashboard')
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+
 module.exports = router;
